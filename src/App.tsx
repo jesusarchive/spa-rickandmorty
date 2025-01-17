9afd64af52c9e941f194141d91b4056a63895a6d
jsur <jsur@gft.com>
2025-01-17 10:28:05 +0100

    setup react query for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-import HomePage from "@/pages/home-page";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import CharacterListPage from "./pages/character-list-page";
+
+const queryClient = new QueryClient();
 
 export default function App() {
-  return <HomePage />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <CharacterListPage />
+    </QueryClientProvider>
+  );
 }
