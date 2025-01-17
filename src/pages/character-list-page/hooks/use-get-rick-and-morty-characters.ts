e9a2ec52bc04265f47351dbcdf51e58e01ec19c3
jsur <jsur@gft.com>
2025-01-17 14:15:05 +0100

    fix

--- src/pages/character-list-page/hooks/use-get-rick-and-morty-characters.ts
+++ src/pages/character-list-page/hooks/use-get-rick-and-morty-characters.ts
@@ -1,6 +1,5 @@
-// import useQuery from "@/hooks/use-query";
-import { useQuery } from "@tanstack/react-query";
-
+import useQuery from "@/hooks/use-query";
+// import { useQuery } from "@tanstack/react-query";
 import getRickAndMortyCharacters from "@/rest-clients/rick-and-morty/get-rick-and-morty-characters";
 import type { CharacterFilters } from "@/rest-clients/rick-and-morty/types";
 
